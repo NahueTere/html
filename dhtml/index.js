953cde8c253f62f5eda6ad88e64d3350d73fc72b
NahueTere <nahuelterenzanito@gmail.com>
2025-05-30 15:21:13 -0300

    j

--- dhtml/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("titulo").innerHTML="Se cambio el titulo"
-let num1= parseInt(prompt("Ingrese el primer numero"))
-let num2= parseInt(prompt("Ingrese segundo numero"))
-
-let suma = num1 + num2
-
-document.getElementById("titulo").innerHTML="La suma es:"+suma
-document.getElementById("titulo").style.color= "#33ff93"
